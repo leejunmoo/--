20cce3984280fca8f7f66ca089d3cc5b5773a80b
leejunmoo <dlquddnr122@naver.com>
2022-11-25 12:47:49 +0900

    2단계 메뉴 액션 및 효과

---
+++ script/main.js
@@ -0,0 +1,16 @@
+
+$(function(){
+
+    $('nav .depth1 > li').hover(
+        function(){
+            //mouseover
+            $(this).children().addClass('show');
+        },
+        function(){
+            //mouseout
+            $(this).children().removeClass('show');
+        }
+    );
+
+
+});
